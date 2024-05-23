d37fa22925de59fd2aef5f5b73c065fe2e16b9fd
tomy08 <tomasesantacruz@gmail.com>
2024-05-23 17:31:01 -0300

    fix: change the order of the routes for preventing errors 🐛

--- src/routes/club.js
+++ src/routes/club.js
@@ -4,7 +4,7 @@ import { getClubs, getClubById, getClubRating } from '../controllers/club.js'
 const router = Router()
 
 router.get('/', getClubs)
-router.get('/:id', getClubById)
 router.get('/rating', getClubRating)
+router.get('/:id', getClubById)
 
 export default router
