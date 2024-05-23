575b7c29f103e22f22ecd1dd6f6712510a7e13ec
tomy08 <tomasesantacruz@gmail.com>
2024-05-23 17:17:32 -0300

    refactor: Update match routes to use controller functions ♻️

--- src/routes/match.js
+++ src/routes/match.js
@@ -1,34 +1,9 @@
 import { Router } from 'express'
-
-import connection from '../db.js'
-import Match from '../models/Match.js'
+import { getMatches, getMatchById } from '../controllers/match.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  connection.query(
-    'SELECT * FROM MatchView ORDER BY match_id ASC;',
-    (err, rows) => {
-      if (err) throw err
-      res.json(Match.fromDbRows(rows))
-    }
-  )
-})
-
-router.get('/:id', (req, res) => {
-  const matchId = req.params.id
-  connection.query(
-    'SELECT * FROM MatchView WHERE match_id = ?;',
-    [matchId],
-    (err, rows) => {
-      if (err) throw err
-      if (rows.length === 0) {
-        res.status(404).json({ message: 'Match not found' })
-        return
-      }
-      res.json(Match.fromDbRow(rows[0]))
-    }
-  )
-})
+router.get('/', getMatches)
+router.get('/:id', getMatchById)
 
 export default router
