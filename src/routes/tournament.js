e361405292a0ff1b74b52315e78871ad5ff55547
tomy08 <tomasesantacruz@gmail.com>
2024-05-23 17:28:46 -0300

    refactor: Update tournament routes to use controller functions ♻️

--- src/routes/tournament.js
+++ src/routes/tournament.js
@@ -1,32 +1,9 @@
 import { Router } from 'express'
-
-import connection from '../db.js'
-
-import Tournament from '../models/Tournament.js'
+import { getTournaments, getTournamentById } from '../controllers/tournament.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  connection.query('SELECT * FROM torneo;', (err, rows) => {
-    if (err) throw err
-    res.json(Tournament.fromDbRows(rows))
-  })
-})
-
-router.get('/:id', (req, res) => {
-  const tournamentId = req.params.id
-  connection.query(
-    'SELECT * FROM torneo WHERE ID = ?;',
-    [tournamentId],
-    (err, rows) => {
-      if (err) throw err
-      if (rows.length === 0) {
-        res.status(404).json({ message: 'Tournament not found' })
-        return
-      }
-      res.json(Tournament.fromDbRow(rows[0]))
-    }
-  )
-})
+router.get('/', getTournaments)
+router.get('/:id', getTournamentById)
 
 export default router
